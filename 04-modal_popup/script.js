122233f10eb391c69467707c09a60a5951d3550d
Arthur Lacerda <arthur.srl@gmail.com>
2024-01-06 19:33:11 -0300

    day 4 `complete` `modal - popup`

---
+++ 04-modal_popup/script.js
@@ -0,0 +1,21 @@
+// variables
+const modal = document.querySelector('.modal'),
+  btn = document.querySelector('.btn'),
+  close = document.querySelector('.close');
+
+btn.addEventListener('click', openModal);
+close.addEventListener('click', closeModal);
+modal.addEventListener('click', closeModal);
+
+// Open modal
+
+function openModal(e) {
+  e.preventDefault();
+  modal.style.display = 'block';
+}
+
+// Close modal
+function closeModal(e) {
+  e.preventDefault();
+  modal.style.display = 'none';
+}
