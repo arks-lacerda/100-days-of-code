99522441ee5e38ead2ce4cfe2ce4fa02471f18ec
Arthur Lacerda <arthur.srl@gmail.com>
2024-01-16 15:57:28 -0300

    13 day of 100 days of code `finished` `Google Direct Link`

--- 13-google_drive_link_generator/script.js
+++ 13-google_drive_link_generator/script.js
@@ -1,8 +1,7 @@
+// Create Variables
 const gLink = document.getElementById('glink');
 const btn = document.getElementById('btn');
 const downloadLink = document.getElementById('download-link');
-const embedAudio = document.getElementById('embed-audio');
-const embedVideo = document.getElementById('embed-video');
 const clear = document.querySelector('.clear');
 
 btn.addEventListener('click', generateLink);
@@ -10,8 +9,9 @@ btn.addEventListener('click', generateLink);
 function generateLink(e) {
   e.preventDefault();
   // console.log(gLink.value);
-  const gLinkValue = document.getElementById('glink').value;
-  const confirmLink = glink.value.includes('https://drive.google.com/file/d/');
+
+  const gLinkValue = gLink.value;
+  const confirmLink = gLink.value.includes('https://drive.google.com/file/d/');
 
   if (confirmLink == true) {
     const getDownloadLink = gLink.value
@@ -19,44 +19,49 @@ function generateLink(e) {
         'https://drive.google.com/file/d/',
         'https://drive.google.com/uc?export=download&id='
       )
-      .replace('/view?usp=sharing', '');
-    // alert(getDownloadLink);
+      .replace('/view?usp=drive_link', '');
+    // console.log(getDownloadLink);
+
     downloadLink.value = getDownloadLink;
 
     function copyText(target) {
       if (target.value == '') {
-        alert('Please generate a Download Link');
+        alert('Please generate a download link');
       } else {
-        target.select();
-        document.execCommand('copy');
-        alert('Link has been copied to clipboard');
+        navigator.clipboard.writeText(target.value).then(() => {
+          alert('Link has been copied to clipboard');
+        });
       }
     }
+
     const copy = document.querySelector('.copy');
     copy.addEventListener('click', () => {
       return copyText(downloadLink);
     });
 
-    // EMBED AUDIO
+    //Embed audio function
     const audio1 = '<audio width="300" height="32" controls="controls" src="';
     const audio2 = '" type="audio/mp3"></audio>';
-
-    console.log(downloadLink.value);
+    const embedAudio = document.getElementById('embed-audio');
     embedAudio.value = `${audio1}${downloadLink.value}${audio2}`;
-    // COPY AUDIO EMBED CODE
+
+    //Copy audio embed code
     const copyAudio = document.querySelector('.copy-audio');
     copyAudio.addEventListener('click', () => {
       return copyText(embedAudio);
     });
 
-    // Embed Video
-    const getVideoLink = gLink.value.replace('/view?usp=sharing', '');
+    //Embed Video function
+    const getVideoLink = gLink.value.replace('/view?usp=drive_link', '');
 
     const video1 = '<iframe src="';
     const video2 = '/preview" width="560" height="315"></iframe>';
 
+    const embedVideo = document.getElementById('embed-video');
+
     embedVideo.value = `${video1}${getVideoLink}${video2}`;
 
+    //Copy video embed code
     const copyVideo = document.querySelector('.copy-video');
     copyVideo.addEventListener('click', () => {
       return copyText(embedVideo);
@@ -64,14 +69,13 @@ function generateLink(e) {
   } else {
     alert('Please Enter a Google Drive File Link');
   }
-}
+  clear.addEventListener('click', clearForm);
 
-clear.addEventListener('click', clearForm);
-
-function clearForm(e) {
-  e.preventDefault();
-  gLink.value = '';
-  downloadLink.value = '';
-  embedAudio.value = '';
-  embedVideo.value = '';
+  function clearForm(e) {
+    e.preventDefault();
+    gLink.value = '';
+    downloadLink.value = '';
+    embedAudio.value = '';
+    embedVideo.value = '';
+  }
 }
